e8b5a1ff526ddca2592bdc940d38c31d7d4e176d
Seoin-Jeong <seoin0529@gmail.com>
2023-05-03 13:04:47 +0900

    23.05.03_1

--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 
-function User({user, onRemove}){
+function User({user, onRemove, onToggle}){
+    useEffect(()=>{
+        console.log('component in');
+        return () => {
+            console.log('component out')
+        };
+    }, []);
     return (
         <div>
             <b style={{
                 cursor: 'pointer',
                 color:user.active ? 'green' : 'black'
-            }}>{user.username}</b> <span>({user.email})</span>
+            }}
+                onClick={()=> onToggle(user.id)}
+            >{user.username}</b> 
+            &nbsp;
+            <span>({user.email})</span>
             <button onClick={()=> onRemove(user.id)}>삭제</button>
-            {/* 고유값인 user.id를 이용해 삭제한다.*/}
+            {/* 고유값인 user.id를 이용해 삭제/수정한다.*/}
         </div>
     );
 }
 
-function UserList({users, onRemove}){
+function UserList({users, onRemove, onToggle}){
     
     return(
         <div>
